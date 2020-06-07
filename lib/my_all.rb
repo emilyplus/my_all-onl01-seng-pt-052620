448f31a71e3e3e1440aa74c927e20bc5523c9569
Emily Jones <emjones829@gmail.com>
2020-06-07 18:29:34 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
 def my_all?(collection)
+  
+  counter = 0
+  
+  block_return_values =[]
+  
+  while counter < collection.legnth
+  
+  bloack_return_values <<
+yield(collection[counter])
+    counter = counter + 1
+  end
+
+  if block_returns_values.include?(false)
+    false
+  else
+    true
+  end
 
 end
